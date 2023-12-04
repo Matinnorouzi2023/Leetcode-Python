c231a38419acea9e7b0211b96e21bced954b6a4d
Matin Hajnorouzi <124842175+Matinnorouzi2023@users.noreply.github.com>
2023-12-04 21:14:05 +0330

    freq_works

---
+++ freq_words.py
@@ -0,0 +1,14 @@
+"INTERVIEW QUESTION - Count the frequency of words appearing in a string Using Python"
+
+
+def freq_words():
+    str=input("please enter your string:")
+    li=str.split(" ")       
+    d={}
+    for i in li:
+       if i not in d.keys():
+          d[i]=0
+       d[i] = d[i] + 1
+    print(d)
+
+freq_words()
